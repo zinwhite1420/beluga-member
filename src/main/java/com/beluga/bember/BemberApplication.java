fc816e737ed0ac50910f17a8735c5c6d30db5b5d
zinwhite1420 <zinwhite1420@gmail.com>
2024-02-20 12:59:01 +0900

    최초 프로젝트 생성

---
+++ src/main/java/com/beluga/bember/BemberApplication.java
@@ -0,0 +1,13 @@
+package com.beluga.bember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beluga/bember/BemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beluga.bember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
