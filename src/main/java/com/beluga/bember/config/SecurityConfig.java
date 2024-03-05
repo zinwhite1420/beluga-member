d6d0157d133ff2343933e8d775818bcd7ed9fabc
zinwhite1420 <zinwhite1420@gmail.com>
2024-03-05 10:52:37 +0900

    JWT 토큰 발급 및 로그인 구현

--- src/main/java/com/beluga/bember/config/SecurityConfig.java
+++ src/main/java/com/beluga/bember/config/SecurityConfig.java
@@ -1,17 +1,41 @@
 package com.beluga.bember.config;
 
+import com.beluga.bember.jwt.JWTUtil;
+import com.beluga.bember.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    //JWTUtil 주입
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -22,26 +46,38 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //csrf disable
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
+
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                    return configuration;
+                })));
         http
                 .csrf(AbstractHttpConfigurer::disable);
 
-        //From 로그인 방식 disable
         http
                 .formLogin(AbstractHttpConfigurer::disable);
 
-        //http basic 인증 방식 disable
         http
                 .httpBasic(AbstractHttpConfigurer::disable);
 
-        //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join","/health").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/login", "/", "/join", "/health").permitAll()
                         .anyRequest().authenticated());
 
-        //세션 설정
+        //AuthenticationManager()와 JWTUtil 인수 전달
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
--- src/main/java/com/beluga/bember/controller/MainController.java
+++ src/main/java/com/beluga/bember/controller/MainController.java
@@ -2,6 +2,7 @@ package com.beluga.bember.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,8 @@ public class MainController {
     private final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
     @GetMapping("/")
     public String mainP() {
-
-        return "main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName(); //현재 세션 사용자 아이디
+        return "Main Controller : " + name;
     }
 
     @GetMapping("/health")
