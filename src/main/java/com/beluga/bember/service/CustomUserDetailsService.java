6b79d99075d9c851416d51adda9887c966407e78
zinwhite1420 <zinwhite1420@gmail.com>
2024-03-05 10:55:30 +0900

    JWT 토큰 발급 및 로그인 구현

---
+++ src/main/java/com/beluga/bember/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.beluga.bember.service;
+
+import com.beluga.bember.domain.UserEntity;
+import com.beluga.bember.dto.CustomUserDetails;
+import com.beluga.bember.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
